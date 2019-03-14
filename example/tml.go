674af914bc7829b25c9300728111fec1c3b6f8b0
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2019-03-14 01:12:42 +0000

    add example

---
+++ example/tml.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+
+	mp3 "github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
+
+	"github.com/romantomjak/shoutcast"
+)
+
+func main() {
+	// open stream
+	stream, err := shoutcast.Open("http://streamingp.shoutcast.com/TomorrowlandOneWorldRadio")
+	if err != nil {
+		panic(err)
+	}
+	defer stream.Close()
+
+	// setup mp3 decoder
+	decoder, err := mp3.NewDecoder(stream)
+	if err != nil {
+		panic(err)
+	}
+	defer decoder.Close()
+
+	// initialise audio player
+	player, err := oto.NewPlayer(decoder.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		panic(err)
+	}
+	defer player.Close()
+
+	// enjoy the music
+	if _, err := io.Copy(player, decoder); err != nil {
+		panic(err)
+	}
+}
