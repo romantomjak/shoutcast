e7fe7d25d138e1136b8842cc85df2e59066fff91
glitch <code@glitch.land>
2023-06-30 11:23:57 -0600

    Remove CLI arg shoutcast URL

--- example/tml.go
+++ example/tml.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
-	"net/url"
-	"os"
 
 	mp3 "github.com/hajimehoshi/go-mp3"
 	"github.com/hajimehoshi/oto"
@@ -13,42 +10,14 @@ import (
 	"github.com/romantomjak/shoutcast"
 )
 
-func parseAndValidateURL() string {
-	urlStr := flag.String("url", "", "URL to process")
-
-	// parse command-line arguments
-	flag.Parse()
-
-	// check if URL argument was provided
-	if *urlStr == "" {
-		fmt.Println("You must specify a URL with the -url flag.")
-		printUsage()
-		os.Exit(1)
-	}
-
-	// check if the URL is valid
-	_, err := url.ParseRequestURI(*urlStr)
-	if err != nil {
-		fmt.Println("Invalid URL provided.")
-		printUsage()
-		os.Exit(1)
-	}
-
-	return *urlStr
-}
-
-func printUsage() {
-	fmt.Println("\nUsage: go run tml.go -url=<your-shoutcast-url>")
-}
+// shoutcast URLs change, you might need to change this to a URL that is live
+const ShoutcastUrl = "https://dancewave.online:443/dance.mp3"
 
 func main() {
-	shoutcastUrl := parseAndValidateURL()
-
-	// continue processing
-	fmt.Println("Processing URL:", shoutcastUrl)
+	fmt.Println("Processing URL:", ShoutcastUrl)
 
 	// open stream
-	stream, err := shoutcast.Open(shoutcastUrl)
+	stream, err := shoutcast.Open(ShoutcastUrl)
 	if err != nil {
 		panic(err)
 	}
